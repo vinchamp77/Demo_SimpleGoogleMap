9555cc691949ae54b38f4eb3c6b409718fa1234a
vinchamp77 <vinchamp77@gmail.com>
2022-05-22 20:27:41 +1000

    Setup basic google MAP API (1) Add library dependencies (2) Add meta-data in AndroidManifest.xml for MAPS_API_KEY (3) Setup 'com.google.android.libraries.mapsplatform.secrets-gradle-plugin' to auto generate the MAPS_API_KEY which is defined in local.properties (4) Basic GoogleMap() composable function

--- app/src/main/java/vtsen/hashnode/dev/simplegooglemapapp/MainActivity.kt
+++ app/src/main/java/vtsen/hashnode/dev/simplegooglemapapp/MainActivity.kt
@@ -3,12 +3,19 @@ package vtsen.hashnode.dev.simplegooglemapapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
 import vtsen.hashnode.dev.simplegooglemapapp.ui.theme.NewEmptyComposeAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +29,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(useSystemUIController: Boolean = true) {
     NewEmptyComposeAppTheme(useSystemUIController = useSystemUIController) {
-        Text(text = stringResource(id = R.string.hello_android))
+        //Text(text = stringResource(id = R.string.hello_android))
+        MapScreen()
     }
+}
+
+@Composable
+fun MapScreen() {
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(44.837789, -0.57918), 12f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+    )
 }
 
 
